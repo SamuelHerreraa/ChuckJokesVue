6b1bc057cacec370a91fbd98ba451afe25e66715
SamuelHerreraa <al02679928@tecmilenio.mx>
2024-04-23 07:41:46 -0500

    Tarea10

---
+++ src/components/ChuckCard.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="card" style="width: 18rem;">
+      <img :src="iconUrl" class="card-img-top" alt="Chuck Norris">
+      <div class="card-body">
+        <p class="card-text">{{ value }}</p>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'ChuckCard',
+    props: {
+      iconUrl: String,
+      value: String
+    }
+  }
+  </script>
+  
+  <style scoped>
+  .card {
+    margin-bottom: 20px;
+  }
+  </style>  
